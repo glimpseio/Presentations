7a1071352a57bb0925fb9d204a05b889c080d1d2
marcprux <marc@glimpse.io>
2019-08-20 09:21:32 -0400

    Fixes for beta6

--- SwiftUI/SwiftUIShapeShifter/SwiftUIShapeShifter/ShapeShifter.swift
+++ SwiftUI/SwiftUIShapeShifter/SwiftUIShapeShifter/ShapeShifter.swift
@@ -1,18 +1,16 @@
-//  ShapeShifter.swift
 //  SwiftUIShapeShifter
-//
 //  Created by Marc Prud'hommeaux on 7/30/19.
 //  https://github.com/glimpseio/Presentations/blob/master/SwiftUI/SwiftUIShapeShifter/SwiftUIShapeShifter/ShapeShifter.swift
 
-/// A model of a shape with a fill and an outline color
-struct ShapeShifterModel : Hashable, Codable {
+/// A model of a shape with a fill and an outline color that can be serialized to data.
+struct DataModel : Hashable, Codable {
     var kind = ShapeKind.circle
-    var fill = HSL(hue: 0.66)
-    var outline = HSL(hue: 0.08)
+    var fill = HSB(hue: 0.66)
+    var outline = HSB(hue: 0.08)
     var outlineWidth = 15.0
 
-    /// A Model of a Color
-    struct HSL : Hashable, Codable {
+    /// A model of a color using hue / saturation / brightness + opacity
+    struct HSB : Hashable, Codable {
         var hue = 0.0
         var saturation = 1.0
         var brightness = 1.0
@@ -28,23 +26,22 @@ struct ShapeShifterModel : Hashable, Codable {
 import SwiftUI
 
 struct ShapeShifterRootView: SwiftUI.View {
-    @EnvironmentObject var env: ShapeShifterEnv
+    @EnvironmentObject var env: DataModel.Container
 
     var body: some SwiftUI.View {
         VStack {
             ShapePreview(model: $env.model).scaledToFit().padding()
-            Divider()
             ShapeInspector(model: $env.model)
         }
     }
 }
 
 struct ShapePreview: SwiftUI.View {
-    @Binding var model: ShapeShifterModel
+    @Binding var model: DataModel
 
     var body: some SwiftUI.View {
         return ZStack {
-            if model.kind == .circle {
+            if model.kind == .circle { // switch statements are unsupported in @ViewBuilder!
                 Circle().fill(model.fill.color)
                 Circle().strokeBorder(model.outline.color, lineWidth: CGFloat(model.outlineWidth))
             } else if model.kind == .square {
@@ -56,37 +53,37 @@ struct ShapePreview: SwiftUI.View {
 }
 
 struct ShapeInspector: SwiftUI.View {
-    @Binding var model: ShapeShifterModel
+    @Binding var model: DataModel
     @State private var showFill = false
     @State private var showOutline = false
 
     var body: some SwiftUI.View {
         VStack {
-            SegmentedControl(selection: $model.kind) {
-                ForEach(ShapeShifterModel.ShapeKind.allCases, id: \.self) { kind in
+            Picker("Kind", selection: $model.kind) {
+                ForEach(DataModel.ShapeKind.allCases, id: \.self) { kind in
                     Text(kind.rawValue.capitalized).tag(kind)
                 }
-            }
+            }.pickerStyle(SegmentedPickerStyle())
 
             Form {
-                HSLSliders(label: "Fill Color", color: $model.fill, shown: $showFill)
-                HSLSliders(label: "Border Color", color: $model.outline, shown: $showOutline)
-                Slider(value: $model.outlineWidth, from: 0, through: 100).padding(15)
+                HSBSliders(label: "Fill Color", color: $model.fill, shown: $showFill)
+                HSBSliders(label: "Border Color", color: $model.outline, shown: $showOutline)
+                Slider(value: $model.outlineWidth, in: 0...100).padding(15)
             }
 
-            UndoRedoButtons()
+            UndoRedoButtons().padding()
         }
     }
 }
 
-/// A collection of sliders that modify a `ShapeShifterModel.HSL` model object.
-struct HSLSliders: SwiftUI.View {
+/// A collection of sliders that modify a `DataModel.HSB` model object.
+struct HSBSliders: SwiftUI.View {
     let label: String
-    @Binding var color: ShapeShifterModel.HSL
+    @Binding var color: DataModel.HSB
     @Binding var shown: Bool
 
     var body: some SwiftUI.View {
-        Section(header: Button(action: { self.$shown.value.toggle() }, label: { Text(label) }).font(.title), footer: SwatchPicker(color: $color)) {
+        Section(header: Button(action: { self.$shown.wrappedValue.toggle() }, label: { Text(label) }).font(.title), footer: SwatchPicker(color: $color)) {
             if shown {
                 PercentSlider(label: "Hue", value: $color.hue)
                 PercentSlider(label: "Saturation", value: $color.saturation)
@@ -99,26 +96,33 @@ struct HSLSliders: SwiftUI.View {
 
 /// A picker for common colors
 struct SwatchPicker: SwiftUI.View  {
-    @Binding var color: ShapeShifterModel.HSL
+    @Binding var color: DataModel.HSB
 
     var body: some SwiftUI.View {
         HStack {
-            ForEach(ShapeShifterModel.HSL.swatches, id: \.self) { swatch in
+            ForEach(DataModel.HSB.swatches, id: \.self) { swatch in
                 Button(action: {
                     self.color = swatch
                 }, label: {
-                    Rectangle()
-                        .fill(swatch.color)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(idealWidth: 50, idealHeight: 50)
+                    Rectangle().fill(swatch.color).aspectRatio(contentMode: .fit)
                 }).border(Color.primary, width: self.color == swatch ? 3 : 0)
             }
         }
     }
 }
 
-extension ShapeShifterModel.HSL {
-    /// Convert the `HSL` model into a `SwiftUI.Color`
+extension DataModel.ShapeKind {
+    /// Creates a path from the current shape kind
+    func createPath() -> SwiftUI.Path {
+        switch self {
+        case .square: return Path(CGRect(x: 1, y: 1, width: 1, height: 1))
+        case .circle: return Path(ellipseIn: CGRect(x: 1, y: 1, width: 1, height: 1))
+        }
+    }
+}
+
+extension DataModel.HSB {
+    /// Convert the `HSB` model into a `SwiftUI.Color`
     var color: SwiftUI.Color {
         Color(hue: hue, saturation: saturation, brightness: brightness, opacity: opacity)
     }
@@ -126,19 +130,18 @@ extension ShapeShifterModel.HSL {
     /// Static constants for pre-defined colors
     static let swatches = [
         Self(saturation: 0.00, brightness: 0.00), // black
-        Self(brightness: 1.00), // red
-        Self(hue: 0.33), // green
-        Self(hue: 0.66), // blue
-        Self(hue: 0.50), // cyan
-        Self(hue: 0.16), // yellow
-        Self(hue: 0.83), // magenta
-        Self(hue: 0.08), // orange
-        Self(hue: 0.83, brightness: 0.50), // purple
         Self(hue: 0.08, saturation: 0.66, brightness: 0.60), // brown
+        Self(brightness: 1.00), // red
+        Self(hue: 0.08), // orange
+        Self(hue: 0.16), // yellow
+        Self(hue: 0.33), // green
+        Self(hue: 0.50), // cyan
+        Self(hue: 0.66), // blue
+        Self(hue: 0.83), // magenta
+        Self(hue: 0.83, brightness: 0.50), // purple
         Self(saturation: 0.00), // white
     ]
 }
-
 
 /// A slider that shows a text label with the value
 struct PercentSlider: SwiftUI.View {
@@ -155,91 +158,81 @@ struct PercentSlider: SwiftUI.View {
     var body: some SwiftUI.View {
         HStack {
             Text(label).frame(minWidth: 100, alignment: .trailing)
-            Slider(value: $value, from: 0.0, through: 1.0)
+            Slider(value: $value, in: 0.0...1.0)
             Text("\(value as NSNumber, formatter: Self.percentFormatter)")
         }
     }
 }
 
 struct UndoRedoButtons: SwiftUI.View {
-    @EnvironmentObject var env: ShapeShifterEnv
+    @EnvironmentObject var env: DataModel.Container
 
     var body: some SwiftUI.View {
         HStack {
             Button("Undo") { self.env.undoManager?.undo() }
-                .disabled(self.env.undoManager?.canUndo == false)
-                .font(.title).buttonStyle(.default)
+                .disabled(self.env.undoManager?.canUndo != true)
             Spacer()
             Button("Redo") { self.env.undoManager?.redo() }
-                .disabled(self.env.undoManager?.canRedo == false)
-                .font(.title).buttonStyle(.default)
+                .disabled(self.env.undoManager?.canRedo != true)
 
-        }.padding()
+        }
+        .font(.title)
+        .buttonStyle(DefaultButtonStyle())
     }
 }
-
-#if DEBUG
-struct ShapeShifterRootView_Previews: PreviewProvider {
-    static var previews: some View {
-        let env = ShapeShifterEnv()
-        env.undoManager = UndoManager()
-        return ShapeShifterRootView()
-            .environmentObject(env)
-            .previewDevice("iPhone SE") // the best device
-    }
-}
-#endif
 
 import Combine
 
-/// The environment in which a `ShapeShifterModel` can be contained.
-final class ShapeShifterEnv : BindableObject {
-    let willChange = Combine.PassthroughSubject<ShapeShifterModel, Never>()
-    var undoManager: UndoManager? = nil
+extension DataModel {
+    /// Model is persisted to user defaults
+    func save(to defaults: UserDefaults = UserDefaults.standard) throws {
+        let shapeData = try JSONEncoder().encode(self)
+        defaults.set(shapeData, forKey: "shapeData")
+        debugPrint("save", String(data: shapeData, encoding: .utf8) ?? "<<ERROR>>")
+    }
 
-    var model = ShapeShifterModel() {
-        willSet {
-            let (oldModel, newModel) = (self.model, newValue)
-            // any changes to the model will be registered with the undo manager
+    mutating func load(from defaults: UserDefaults = UserDefaults.standard) throws -> Bool {
+        if let data = defaults.data(forKey: "shapeData") {
+            self = try JSONDecoder().decode(Self.self, from: data)
+            return true
+        } else {
+            return false // no saved data to load
+        }
+    }
+
+    /// A reference container for a DataModel that can publishes changes and handles undo registration
+    class Container : Combine.ObservableObject {
+        @Published var model = DataModel() {
+            willSet { registerUndo(state: self.model) }
+            didSet { scheduleSave() }
+        }
+
+        /// The undoManager associated with the environment
+        var undoManager: UndoManager? = nil
+
+        /// Register a state change with the current UndoManager
+        private func registerUndo(state lastModel: DataModel) {
             if let um = self.undoManager, um.isUndoRegistrationEnabled {
                 um.registerUndo(withTarget: self) { target in
-                    withAnimation(Animation.spring(blendDuration: 2.5)) {
-                        target.model = oldModel
+                    withAnimation(.easeInOut(duration: 0.5)) { // because YOLO
+                        target.model = lastModel
                     }
                 }
             }
-
-            willChange.send(newModel) // inform listeners of changes
         }
 
-        didSet {
+        /// The current outstanding work item to save
+        private var saveWorkItem: DispatchWorkItem? = nil
+
+        /// Save the data model to the `UserDefaults` after a delay (with throttling).
+        private func scheduleSave(after deadline: DispatchTime = .now() + 2.0) {
             // serialize the data to the `UserDefaults` in the background
             let saveData = self.model
             let item = DispatchWorkItem { try? saveData.save() }
             self.saveWorkItem?.cancel() // cancel any previous save request (e.g., debounce)
             self.saveWorkItem = item
             // save to user defaults in the background
-            DispatchQueue.global(qos: .background).async(execute: item)
-        }
-    }
-
-    /// The current outstanding work item to save
-    private var saveWorkItem: DispatchWorkItem? = nil
-}
-
-extension ShapeShifterModel {
-    /// Model is persisted to user defaults
-    func save() throws {
-        let shapeData = try JSONEncoder().encode(self)
-        UserDefaults.standard.set(shapeData, forKey: "shapeData")
-    }
-
-    mutating func load() throws -> Bool {
-        if let data = UserDefaults.standard.data(forKey: "shapeData") {
-            self = try JSONDecoder().decode(Self.self, from: data)
-            return true
-        } else {
-            return false // no saved data to load
+            DispatchQueue.global(qos: .background).asyncAfter(deadline: deadline, execute: item)
         }
     }
 }
@@ -255,18 +248,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    let store = ShapeShifterEnv()
+    let store = DataModel.Container()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let _ = try? store.model.load() // load from defaults, ignoring erros
+            let _ = try? store.model.load() // load data from defaults, ignoring erros
             let rootView = ShapeShifterRootView().environmentObject(store)
             window.rootViewController = UIHostingController(rootView: rootView)
             self.window = window
             window.makeKeyAndVisible()
-            // undo manager is created only when the window is shown
             store.undoManager = window.undoManager
         }
     }
 }
+
+#if DEBUG
+struct ShapeShifterRootView_Previews: PreviewProvider {
+    static var previews: some View {
+        let env = DataModel.Container()
+        env.undoManager = UndoManager()
+        return ShapeShifterRootView()
+            .environmentObject(env)
+            .previewDevice("iPhone SE") // the best device
+    }
+}
+#endif
